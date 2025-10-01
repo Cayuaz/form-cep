f568f3490895cad6b461415f875520a6ce5f6bd5
Cayuã <cayuaronald7@gmail.com>
2025-10-01 09:53:34 -0400

    refactor: separate scripts in modules: index, logic and display

--- scripts/index.js
+++ scripts/index.js
@@ -1,130 +1,21 @@
-//error container function
-//input cep event
-//check input cep value
-//fetch cep
-//iteration of result cep
-//Iterar os inputs com o atributo data-input, e pegar o atributo de cada um deles para usar no switch e colocar o valor certinho com base no obj retornado pelo fetch
+import {checkInput, getValidInput, execute} from "./logic.js";
+import {removeErrorContainer} from "./display.js";
 
-// function checkObj(obj){
-//     for(prop in obj){
-//         const propValue = obj[prop]
-//         if(propValue === "undefined" || propValue === null){
-//             return false
-//         }  
-//     }
-//     return true
-// }
-
-function displayCep(obj){
-    const inputs = document.querySelectorAll("[data-input]")
-    Array.from(inputs).forEach(input => {
-        const attribute = input.getAttribute("data-input")
-
-        switch(attribute){
-            case "street": 
-                input.value = obj.logradouro;
-                break;
-            case "district":
-                input.value = obj.bairro;
-                break;
-            case "city":
-                input.value = obj.localidade;
-                break;
-            case "uf":
-                input.value = obj.uf;
-                break;
-            default: 
-                break;
-        }
-    })
-}
-
-async function findCep(cep){
-    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-        if(response.ok){
-            const result = await response.json()
-            console.log(result)
-            if(result.erro){
-                throw new Error("CEP não encontrado")  
-            }
-
-            return result
-        } else {
-            throw new Error("Erro na requisição. Verifique sua conexão ou o CEP.")    
-        }
-}
-
-// function getCep(input){
-//     const [part1, part2] = input.value.split("-")
-//     const cepString = part1 + part2
-//     return parseInt(cepString)
-// }
-
-const errorContainer = document.querySelector(".error-container");
-const errorBtn = errorContainer.querySelector("button");
-
-const removeErrorContainer = () => {
-        errorContainer.classList.remove("display-error-container");
-        errorBtn.removeEventListener("click", removeErrorContainer)
-}
-
-function displayError(error){
-    const paragraphError = errorContainer.querySelector("p")
-    paragraphError.textContent = error
-    errorContainer.classList.add("display-error-container");
-
-    errorBtn.focus();
-
-    errorBtn.addEventListener("click", () => {
-        removeErrorContainer()
-    });
-}
-
-async function execute(input){
-    setTimeout( async () => {  
-            try {
-
-                const addressObj = await findCep(input)
-                console.log(addressObj)
-                displayCep(addressObj)
-                
-                console.log("teste de execute");
-            } catch (error) {
-                displayError(error.message);
-            }
-
-    }, 2000)
-}
-
-const getValidInput = input => input.value.replace("-", "").trim() 
-
-function checkInput(input){
-
-    const validInput = getValidInput(input)
-    if(validInput.length === 8){
-        return true
-    } else {
-        false
-    }
-
-}
-   
-const cepInput = document.getElementById("cep")
-
-
+const cepInput = document.getElementById("cep");
+const form = document.getElementById("form");
 
 cepInput.addEventListener("input", () => {
 
-    if(checkInput(cepInput)){
-        //Remove o traço para não passar um input com um valor estranho mesmo que ele passe na verficação
-        const validInput = getValidInput(cepInput)
+    if(checkInput(cepInput)){;
+
+        //Chama a função getValidInput para formatar o valor do input e evitar caracteres que não sejam números
+        const validInput = getValidInput(cepInput);
         execute(validInput);
     }
 
-    removeErrorContainer()
+    removeErrorContainer();
 })
 
-const form = document.getElementById("form")
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
---
+++ scripts/logic.js
@@ -0,0 +1,50 @@
+import {displayError, displayCep} from "./display.js";
+
+//Funçaõ que faz a requisição dos endereços com base no CEP
+async function findCep(cep){
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        //Se os dados estiverem corretos chama response.json e converte os dados
+        if(response.ok){
+            const result = await response.json();
+            //Verifica se existe alguma propriedade chamada "erro"
+            if(result.erro){
+                throw new Error("CEP não encontrado");
+            }
+
+            return result
+        } else {
+            throw new Error("Erro na requisição. Verifique sua conexão ou o CEP.");
+        }
+}
+
+//Função primcipal, que controla a execução com base nos resultados de findCep
+async function execute(input){
+    setTimeout( async () => {  
+            try {
+                const addressObj = await findCep(input);
+                //Se findCep não disparar nenhum erro, ele chama a função exibe os endereços 
+                displayCep(addressObj);
+            } catch (error) {
+                //Caso findCep tenha algum erro, ela intercepta ele e chama a função que exibe o container de erro 
+                displayError(error.message);
+            }
+
+    }, 2000)
+}
+
+//Remove quaisquer caracteres que não sejam números do valor digitado no input
+const getValidInput = input => input.value.replace(/\D/g, "");
+
+//Verifica se a quantidade de caracteres do valor digitado no input é maior que 8
+function checkInput(input){
+
+    const validInput = getValidInput(input);
+    if(validInput.length === 8){
+        return true;
+    } else {
+        false;
+    }
+
+}
+
+export {checkInput, getValidInput, execute};
